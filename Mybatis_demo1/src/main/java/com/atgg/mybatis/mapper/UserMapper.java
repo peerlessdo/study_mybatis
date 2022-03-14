1eea76819234e2293b3cbc095289e2ee657a3633
wang.zhe <945443730@qq.com>
2022-03-14 15:09:36 +0800

    提交增删查改操作

--- Mybatis_demo1/src/main/java/com/atgg/mybatis/mapper/UserMapper.java
+++ Mybatis_demo1/src/main/java/com/atgg/mybatis/mapper/UserMapper.java
@@ -1,6 +1,14 @@
 package com.atgg.mybatis.mapper;
 
+import com.atgg.mybatis.pojo.User;
+
 public interface UserMapper {
 
     int insertUser();
+
+    int deleteUser();
+
+    User selectUser();
+
+    int updateUser();
 }
--- Mybatis_demo1/src/test/java/com/atgg/mybatis/test/MybatisTest.java
+++ Mybatis_demo1/src/test/java/com/atgg/mybatis/test/MybatisTest.java
@@ -1,6 +1,7 @@
 package com.atgg.mybatis.test;
 
 import com.atgg.mybatis.mapper.UserMapper;
+import com.atgg.mybatis.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,7 +14,7 @@ import java.io.InputStream;
 public class MybatisTest {
 
     @Test
-    public void testMybatis() throws IOException {
+    public void testMybatisInsert() throws IOException {
         //加载核心配置文件
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
         //获取SqlSessionFactoryBuilder
@@ -26,6 +27,63 @@ public class MybatisTest {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         //测试功能
         int result = userMapper.insertUser();
+        //事务提交
+//        sqlSession.commit();
+        System.out.println("result: " + result);
+    }
+
+    @Test
+    public void testMybatisDelete() throws IOException {
+        //加载核心配置文件
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        //获取SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        //获取SqlSessionFactory（是生产SqlSession的工厂）
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+        //获取SqlSession（代表Java程序与数据库之间的会话，HttpSession是Java程序与浏览器之间的会话）
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获取mapper接口对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        //测试功能
+        int result = userMapper.deleteUser();
+        //事务提交
+//        sqlSession.commit();
+        System.out.println("result: " + result);
+    }
+
+    @Test
+    public void testMybatisSelect() throws IOException {
+        //加载核心配置文件
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        //获取SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        //获取SqlSessionFactory（是生产SqlSession的工厂）
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+        //获取SqlSession（代表Java程序与数据库之间的会话，HttpSession是Java程序与浏览器之间的会话）
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获取mapper接口对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        //测试功能
+        User user = userMapper.selectUser();
+        //事务提交
+//        sqlSession.commit();
+        System.out.println("user：" + user);
+    }
+
+    @Test
+    public void testMybatisUpdate() throws IOException {
+        //加载核心配置文件
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        //获取SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        //获取SqlSessionFactory（是生产SqlSession的工厂）
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+        //获取SqlSession（代表Java程序与数据库之间的会话，HttpSession是Java程序与浏览器之间的会话）
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获取mapper接口对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        //测试功能
+        int result = userMapper.updateUser();
         //事务提交
 //        sqlSession.commit();
         System.out.println("result: " + result);
